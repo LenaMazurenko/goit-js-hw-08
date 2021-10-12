3b0cb66cefa74f8659815bd8d9b83849e641e03c
LenaMazurenko <86721157+LenaMazurenko@users.noreply.github.com>
2021-10-12 10:03:41 +0000

    Deploying to gh-pages from @ LenaMazurenko/goit-js-hw-08@d9cf62e964faee8f3e46ed989175c21941209f0b 🚀

--- 01-gallery.5e570cfe.js
+++ 01-gallery.5e570cfe.js
@@ -8,4 +8,4 @@ var t=arguments[3];function e(t){return(e="function"==typeof Symbol&&"symbol"==t
 },{}],"TfQ5":[function(require,module,exports) {
 "use strict";var e=require("./gallery-items"),l=i(require("simplelightbox"));function i(e){return e&&e.__esModule?e:{default:e}}require("simplelightbox/dist/simple-lightbox.min.css");const r=document.querySelector(".gallery"),t=n(e.galleryItems);r.insertAdjacentHTML("beforeend",t);const a=new l.default(".gallery a",{captionsData:"alt",captionDelay:250});function n(e){return e.map(({preview:e,original:l,description:i})=>`<a class="gallery__item" href="${l}">\n  <img class="gallery__image" src="${e}" alt="${i}" />\n</a>`).join("")}
 },{"./gallery-items":"q5Rn","simplelightbox":"yx6k","simplelightbox/dist/simple-lightbox.min.css":"kz9M"}]},{},["TfQ5"], null)
-//# sourceMappingURL=/parcel-project-template/01-gallery.5e570cfe.js.map
+//# sourceMappingURL=/goit-js-hw-08/01-gallery.5e570cfe.js.map
--- 02-video.e7667b36.js
+++ 02-video.e7667b36.js
@@ -4,4 +4,4 @@ var e=arguments[3];function t(e,t){if(!(e instanceof t))throw new TypeError("Can
 },{}],"iiAF":[function(require,module,exports) {
 "use strict";var e=o(require("@vimeo/player"));function o(e){return e&&e.__esModule?e:{default:e}}require(["https://player.vimeo.com/api/player.js"],function(e){new e(document.querySelector("#vimeo-player")).on("play",function(){console.log("played the video!")})}),nn;
 },{"@vimeo/player":"zU8P"}]},{},["iiAF"], null)
-//# sourceMappingURL=/parcel-project-template/02-video.e7667b36.js.map
+//# sourceMappingURL=/goit-js-hw-08/02-video.e7667b36.js.map
