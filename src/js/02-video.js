a7a8dcde4099c96f393dc2282f44cebb18a2e03f
LenaMazurenko <lena@rent-it.su>
2021-10-12 14:20:04 +0300

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,33 @@
 import Player from '@vimeo/player';
 
-require(['https://player.vimeo.com/api/player.js'], function (Player) {
-  var iframe = document.querySelector('#vimeo-player');
-  var player = new Player(iframe);
-
-  player.on('play', function () {
-    console.log('played the video!');
-  });
+const player = new Player('vimeo-player', {
+  // id: 19231868,
+  width: 640,
 });
-nn;
+
+var onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+};
+
+player.on('timeupdate', onPlay);
+
+const savedSettings = localStorage.getItem('videoplayer-current-time');
+const parsedSettings = JSON.parse(savedSettings);
+const stopTime = parsedSettings.seconds;
+
+player
+  .setCurrentTime(stopTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
